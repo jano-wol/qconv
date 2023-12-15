c8a97766237ad389d40f5218912335bace0f75ca
Andrzej Polak <jjjkamu@gmail.com>
2023-12-15 14:35:25 +0100

    Call simdops conv

--- source/test/bin/test_qconv/source/main.cpp
+++ source/test/bin/test_qconv/source/main.cpp
@@ -23,10 +23,13 @@ void testQConv()
   constexpr int SpatialOut = 16;
   alignas(NativeAlignment) int8_t input[SpatialIn * 20 * 20];
   alignas(NativeAlignment) int16_t weights[SpatialIn * SpatialOut * 3 * 3];
+  QConv<SpatialIn, SpatialOut, 20, 3> q;
   QConvNaive<SpatialIn, SpatialOut, 20, 3> qN;
 
   constInit(input, SpatialIn * 20 * 20, static_cast<int8_t>(1));
   constInit(weights, SpatialIn * SpatialOut * 3 * 3, static_cast<int16_t>(1));
+  q.initWeights(weights);
+  q.propagate(input);
   qN.initWeights(weights);
   qN.propagate(input);
   checkTest(qN.outputBuf[0], 64, contextN);
@@ -41,6 +44,8 @@ void testQConv()
 
   int16_t weightsNew[9] = {-90, -80, -70, -60, 5, 1, 2, 3, 4};
   std::memcpy(weights, weightsNew, 9 * sizeof(int16_t));
+  q.initWeights(weights);
+  q.propagate(input);
   qN.initWeights(weights);
   qN.propagate(input);
   checkTest(qN.outputBuf[0], 73, contextN);
@@ -51,15 +56,19 @@ void testQConv()
 
   constInit(weights, SpatialIn * SpatialOut * 3 * 3, static_cast<int16_t>(1));
   std::memcpy(weights + 5 * 9, weightsNew, 9 * sizeof(int16_t));
+  q.initWeights(weights);
+  q.propagate(input);
   qN.initWeights(weights);
-  qN.propagate(input);
+  qN.propagate(input);  
   checkTest(qN.outputBuf[0], 73, contextN);
   checkTest(qN.outputBuf[5 * 20 * 20 + 210], 144, contextN);
 
   constInit(weights, SpatialIn * SpatialOut * 3 * 3, static_cast<int16_t>(1));
   std::memcpy(weights + 16 * 5 * 9, weightsNew, 9 * sizeof(int16_t));
+  q.initWeights(weights);
+  q.propagate(input);
   qN.initWeights(weights);
-  qN.propagate(input);
+  qN.propagate(input);  
   checkTest(qN.outputBuf[0], 64, contextN);
   checkTest(qN.outputBuf[5 * 20 * 20 + 19], 10, contextN);
   checkTest(qN.outputBuf[5 * 20 * 20 + 210], -150, contextN);
@@ -74,6 +83,8 @@ void testQConv()
     }
   }
   std::memcpy(input, inputLayer, 20 * 20 * sizeof(int8_t));
+  q.initWeights(weights);
+  q.propagate(input);
   qN.initWeights(weights);
   qN.propagate(input);
   checkTest(qN.outputBuf[0], -132, contextN);
