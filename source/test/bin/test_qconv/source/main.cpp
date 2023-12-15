7b961e2c6c9b8b42e9df3127a6507e348d11abaf
Andrzej Polak <jjjkamu@gmail.com>
2023-12-15 12:12:39 +0100

    Add test

--- source/test/bin/test_qconv/source/main.cpp
+++ source/test/bin/test_qconv/source/main.cpp
@@ -7,8 +7,17 @@ using namespace qconv::core;
 using namespace qconv::layers;
 using namespace qconv::simdops;
 
+void checkTest(int val, int expectedVal, const std::string& context)
+{
+  if (val != expectedVal) {
+    std::cerr << "Test fail. val=" << val << " expectedVal=" << expectedVal << " context=" << context << "\n";
+    exit(1);
+  }
+}
+
 void testQConvNaive()
 {
+  std::string context = "testQConvNaive";
   constexpr int SpatialIn = 16;
   constexpr int SpatialOut = 16;
   alignas(NativeAlignment) int8_t input[SpatialIn * SpatialOut * 20 * 20];
@@ -17,6 +26,10 @@ void testQConvNaive()
   constInit(weights, SpatialIn * SpatialOut * 3 * 3, static_cast<int16_t>(1));
   QConvNaive<SpatialIn, SpatialOut, 20, 3> q;
   q.initWeights(weights);
+
+  q.propagate(input);
+
+  checkTest(q.outputBuf[0], 64, context);
 }
 
 int main()
