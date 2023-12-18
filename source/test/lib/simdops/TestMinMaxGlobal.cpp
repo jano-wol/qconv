f48ba38adbd59f8e6914ab5501f03e53b64364c1
Andrzej Polak <jjjkamu@gmail.com>
2023-12-18 16:25:34 +0100

    Add MinMaxGlobal test

---
+++ source/test/lib/simdops/TestMinMaxGlobal.cpp
@@ -0,0 +1,17 @@
+#include <gmock/gmock.h>
+
+#include <simdops/simdops.h>
+#include <testutils/testutils.h>
+
+using namespace qconv;
+using namespace qconv::simdops;
+using namespace qconv::testutils;
+
+TEST(MinMaxGlobal, Test1)
+{
+  alignas(simdops::NativeAlignment) int8_t a[64];
+  modInit(a, 64, 8);
+  a[59] = -7;
+  int8_t ret = simdops::minGlobal<64>(a);
+  EXPECT_EQ(ret, -7);
+}
