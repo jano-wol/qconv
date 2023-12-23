e0dac6079cac91b1f822a1f06512c8d1a3f83aff
janowol <janos.wolosz@gmail.com>
2023-12-23 20:59:41 +0100

    Make avx2 and clang default

--- source/scripts/build/init.sh
+++ source/scripts/build/init.sh
@@ -4,15 +4,7 @@ if [[ $1 != "debug" ]] && [[ $1 != "release" ]] && [[ $1 != "relwithdebinfo" ]];
     echo "First command line argument should be debug or release or relwithdebinfo"
     exit 1
 fi	
-if [[ $2 != "" ]] && [[ $2 != "oldarch" ]]; then
-    echo "Second command line argument should be <empty string> or oldarch"
-    exit 1
-fi	
 CMAKE_BUILD_TYPE=$1
-CMAKE_BUILD_ARCH="avx2"
-if [[ $2 == "oldarch" ]]; then
-    CMAKE_BUILD_ARCH="oldarch"
-fi	
 SCRIPT_FOLDER=$(dirname "${0}") 
 WORKSPACE_FOLDER=$(readlink -e "${SCRIPT_FOLDER}/../../")
 BUILD_FOLDER="${WORKSPACE_FOLDER}/build/${CMAKE_BUILD_TYPE}"
@@ -20,5 +12,5 @@ BUILD_DATA_FOLDER="${WORKSPACE_FOLDER}/build/${CMAKE_BUILD_TYPE}/data"
 BUILD_TEST_DATA_FOLDER="${WORKSPACE_FOLDER}/build/${CMAKE_BUILD_TYPE}/test/data"
 SOURCE_FOLDER="${WORKSPACE_FOLDER}/source"
 PYTHON_FOLDER="${SOURCE_FOLDER}/python"
-CMAKE_CXX_COMPILER=g++
+CMAKE_CXX_COMPILER=clang++
 CMAKE_MAKE_PROGRAM=ninja
