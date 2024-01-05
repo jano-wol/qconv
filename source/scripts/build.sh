a9e0606f70839d0bfdc458288357baf88b1cd8f1
janowol <janos.wolosz@gmail.com>
2024-01-05 12:44:46 +0100

    Silence objdump debug warning

--- source/scripts/build.sh
+++ source/scripts/build.sh
@@ -2,4 +2,7 @@
 set -ex
 source "$(dirname "${0}")/build/init.sh"
 cmake --build "${BUILD_FOLDER}" -- -j 4 -v
-source "$(dirname "${0}")/misc/objdump/objdump.sh"
+if [ "$1" = "release" ]; then
+  source "$(dirname "${0}")/misc/objdump/objdump.sh"
+fi
+echo "Build is ready."
