52b1ca1ac971b9501374c252e3da05fd31ab1008
janowol <janos.wolosz@gmail.com>
2024-01-05 10:39:34 +0100

    Fix linear layer

--- source/lib/layers/include/layers/linear.h
+++ source/lib/layers/include/layers/linear.h
@@ -2,31 +2,24 @@
 #define QCONV_LAYERS_LINEAR_H_INCLUDED
 
 #include <cassert>
+#include <cstring>
 
 #include <simd/simd.h>
 
-static inline __m128i hsumI32x4(__m256i sum0, __m256i sum1, __m256i sum2, __m256i sum3)
+static inline int32_t hsum_epi32(__m128i x)
 {
-  sum0 = _mm256_hadd_epi32(sum0, sum1);
-  sum2 = _mm256_hadd_epi32(sum2, sum3);
-  sum0 = _mm256_hadd_epi32(sum0, sum2);
-
-  __m128i sum128lo = _mm256_castsi256_si128(sum0);
-  __m128i sum128hi = _mm256_extracti128_si256(sum0, 1);
-  __m128i sum128 = _mm_add_epi32(sum128lo, sum128hi);
-
-  return sum128;
+  __m128i hi64 = _mm_unpackhi_epi64(x, x);
+  __m128i sum64 = _mm_add_epi32(hi64, x);
+  __m128i hi32 = _mm_shuffle_epi32(sum64, _MM_SHUFFLE(2, 3, 0, 1));  // Swap the low two elements
+  __m128i sum32 = _mm_add_epi32(sum64, hi32);
+  return _mm_cvtsi128_si32(sum32);  // movd
 }
 
-static inline void add_dpbusd_epi32(__m256i& acc, __m256i a, __m256i b)
+static inline int32_t hsum_8x32(__m256i v)
 {
-  // #if defined(USE_VNNI)
-  //   This does exactly the same thing as explained below but in one instruction.
-  //   acc = _mm256_dpbusd_epi32(acc, a, b);
-  __m256i product = _mm256_maddubs_epi16(a, b);
-  product = _mm256_madd_epi16(product, _mm256_set1_epi16(1));
-  acc = _mm256_add_epi32(acc, product);
-};
+  __m128i sum128 = _mm_add_epi32(_mm256_castsi256_si128(v), _mm256_extracti128_si256(v, 1));
+  return hsum_epi32(sum128);
+}
 
 namespace qconv::layers
 {
@@ -57,39 +50,20 @@ public:
     assert(simd::isPtrAligned(input));
     assert(simd::isPtrAligned(weights));
     assert(simd::isPtrAligned(biases));
-    static_assert(OutSize % 4 == 0, "OutSize must be divisble by 4");
-    constexpr int OutNumBatches = OutSize / 4;
-    for (int i = 0; i < OutNumBatches; i++) {
-      // Prepare weight offsets. One offset for one row of weights.
-      // This is a simple index into a 2d array.
-      const int offset0 = (i * 4 + 0) * InSize;
-      const int offset1 = (i * 4 + 1) * InSize;
-      const int offset2 = (i * 4 + 2) * InSize;
-      const int offset3 = (i * 4 + 3) * InSize;
-
-      // Accumulation starts from 0, we add the bias only at the end.
-      auto sum0 = _mm256_setzero_si256();
-      auto sum1 = _mm256_setzero_si256();
-      auto sum2 = _mm256_setzero_si256();
-      auto sum3 = _mm256_setzero_si256();
-
-      // Each innermost loop processes a 32x4 chunk of weights, so 128 weights at a time!
-      constexpr int StepWidth = (simd::RegisterWidth / 8) / sizeof(int8_t);
-      for (size_t j = 0; j < InSize / StepWidth; j++) {
-        auto in = _mm256_load_si256(reinterpret_cast<const __m256i*>(input + j * StepWidth));
-        add_dpbusd_epi32(sum0, in,
-                         _mm256_load_si256(reinterpret_cast<const __m256i*>(weights[0] + offset0 + j * StepWidth)));
-        add_dpbusd_epi32(sum1, in,
-                         _mm256_load_si256(reinterpret_cast<const __m256i*>(weights[0] + offset1 + j * StepWidth)));
-        add_dpbusd_epi32(sum2, in,
-                         _mm256_load_si256(reinterpret_cast<const __m256i*>(weights[0] + offset2 + j * StepWidth)));
-        add_dpbusd_epi32(sum3, in,
-                         _mm256_load_si256(reinterpret_cast<const __m256i*>(weights[0] + offset3 + j * StepWidth)));
+    std::memcpy(outputBuf, biases, OutSize * sizeof(OutputType));
+    static_assert(InSize % 8 == 0, "InSize must be divisble by 8");
+    for (size_t inIdx = 0; inIdx < InSize; inIdx += 8) {
+      auto inputChunk = _mm256_setr_epi32(input[inIdx], input[inIdx + 1], input[inIdx + 2], input[inIdx + 3],
+                                          input[inIdx + 4], input[inIdx + 5], input[inIdx + 6], input[inIdx + 7]);
+      for (size_t outIdx = 0; outIdx < OutSize; ++outIdx) {
+        auto weightsChunk =
+            _mm256_setr_epi32(weights[outIdx][inIdx], weights[outIdx][inIdx + 1], weights[outIdx][inIdx + 2],
+                              weights[outIdx][inIdx + 3], weights[outIdx][inIdx + 4], weights[outIdx][inIdx + 5],
+                              weights[outIdx][inIdx + 6], weights[outIdx][inIdx + 7]);
+        auto x = _mm256_mullo_epi32(inputChunk, weightsChunk);
+        auto y = hsum_8x32(x);
+        outputBuf[outIdx] += y;
       }
-
-      auto outval = hsumI32x4(sum0, sum1, sum2, sum3);
-      outval = _mm_add_epi32(outval, _mm_loadu_si128(reinterpret_cast<const __m128i*>(biases + i * 4)));
-      _mm_storeu_si128(reinterpret_cast<__m128i*>(outputBuf + i * 4), outval);
     }
   }
 
